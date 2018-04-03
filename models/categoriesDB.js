127e132168fa27244926de178d92bbd1b74517f3
Nian Liu <nianliu@ool-ad0205c2.dyn.optonline.net>
2018-04-03 17:54:18 -0400

    added categories model controller routes and added in serverjs

--- models/categoriesDB.js
+++ models/categoriesDB.js
@@ -6,7 +6,7 @@ module.exports = {
     return db.many(`
       SELECT *
       FROM categories
-      ORDER BY id DESC
+      ORDER BY id ASC
     `);
   },
 
--- routes/categoryroute.js
+++
@@ -1,3 +0,0 @@
-const express = require('express');
-
-const categoryRouter = express.Router();
