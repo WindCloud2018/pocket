3e0aaa358d4b34587f6ef2b17a7830ac23099c23
Carson Chen <caiyongchen@live.com>
2018-04-10 13:35:27 -0400

    finish debug

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,15 +4,13 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-left">
-        <h4>Authors</h4>
-        <ul>
-          <li><i class="fab fa-github"></i> Carson Chen </li>
-          <li><i class="fab fa-github"></i> Nian Liu</li>
-        </ul>
+
+      <div className="footer-top">
+        <a href="https://github.com/WindCloud2018/pocket" ><i class="fab fa-github-square fa-3x"></i></a>
       </div>
-      <div className="footer-right">
-        Right
+
+      <div className="footer-bottom">
+        <p>By The WindCloud Team</p>
       </div>
     </div>
   );
